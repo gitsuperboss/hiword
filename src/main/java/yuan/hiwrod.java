1ef8ff40d491ea29df1f6691c0406bd9dbae24b1
gitsuperboss <55531886+gitsuperboss@users.noreply.github.com>
2019-10-26 09:42:49 +0800

    Update hiwrod.java

--- src/main/java/yuan/hiwrod.java
+++ src/main/java/yuan/hiwrod.java
@@ -2,6 +2,6 @@ package yuan;
 
 public class hiwrod {
     public static void main(String[] args) {
-        System.out.println("NO Hi word");
+        System.out.println("NO NO Hi word");
     }
 }
