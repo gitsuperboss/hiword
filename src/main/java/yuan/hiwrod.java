2543dab011735266608bbf75164ee590deb3d2ff
gitsuperboss <55531886+gitsuperboss@users.noreply.github.com>
2019-10-26 14:49:44 +0800

    Update hiwrod.java

--- src/main/java/yuan/hiwrod.java
+++ src/main/java/yuan/hiwrod.java
@@ -3,5 +3,7 @@ package yuan;
 public class hiwrod {
     public static void main(String[] args) {
         System.out.println("王培龙真是个大傻逼");
+         System.out.println("王培龙是个大傻逼");
+         System.out.println("王培龙就是个大傻逼");
     }
 }
